81d30222a42a22b79985d5364180dcd666bf9e79
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-02-08 17:22:25 +0500

    fix(coinbase/address/schema.mjs): update schema to match changed response

--- response/coinbase/address/schema.mjs
+++ response/coinbase/address/schema.mjs
@@ -1,19 +1,81 @@
 /**
  * Address related JSON-schemas for Coinbase Advanced API response structure.
  *
+ * @version 2025-02-01
  * @see https://json-schema.org/draft/2020-12/release-notes
  * @module response/coinbase/address/schema
  */
 
+import collection from '../../../collection/coinbase/address_all.json' with { type: 'json' };
+import config from '#config/coinbase.json' with { type: 'json' };
+import { composePattern } from '#lib/regexp.mjs';
+
 const address = {
-  properties: {
-    address: { type: 'string' },
-    id: { type: 'string' },
-    network: { type: 'string' },
+    additionalProperties: false,
+    type: 'object',
+    properties: {
+      address: {
+        /** @todo Bitcoin, Solana. */
+        pattern: composePattern('EVM_ADDRESS'),
+        type: 'string',
+      },
+      created_at: {
+        pattern: composePattern('DATE_ISO'),
+        type: 'string',
+      },
+      currency: {
+        enum: collection.map((item) => item.currency),
+        type: 'string',
+      },
+      id: {
+        pattern: composePattern('UUID'),
+        type: 'string',
+      },
+      name: { type: ['null', 'string'] },
+      network: {
+        enum: collection.map((item) => item.network),
+        type: 'string',
+      },
+      resource: {
+        enum: ['addresses'],
+        type: 'string',
+      },
+      resource_path: {
+        pattern: composePattern(
+          {
+            account_uuid: 'UUID',
+            address_uuid: 'UUID',
+          },
+          config.PATH.ADDRESS_ONE,
+        ),
+        type: 'string',
+      },
+      updated_at: {
+        pattern: composePattern('DATE_ISO'),
+        type: 'string',
+      },
+    },
+    required: [
+      'address',
+      'currency',
+      'created_at',
+      'id',
+      'network',
+      'resource',
+      'resource_path',
+      'updated_at',
+    ],
   },
-  required: ['address', 'id', 'network'],
-  type: 'object',
-};
+  pagination = {
+    properties: {
+      ending_before: { type: ['null', 'string'] },
+      limit: { type: 'number' },
+      order: { enum: ['asc', 'desc'], type: 'string' },
+      starting_after: { type: ['null', 'string'] },
+    },
+    required: ['limit', 'order'],
+    type: 'object',
+  };
 
 export const addressAll = {
   $schema: 'https://json-schema.org/draft/2020-12/schema',
@@ -24,7 +86,7 @@ export const addressAll = {
       minItems: 1,
       type: 'array',
     },
-    pagination: { type: 'object' },
+    pagination,
   },
   required: ['data', 'pagination'],
   type: 'object',
@@ -35,6 +97,7 @@ export const addressNew = {
   additionalProperties: false,
   properties: {
     data: address,
+    pagination,
   },
   required: ['data'],
   type: 'object',
@@ -45,7 +108,7 @@ export const addressOne = {
   additionalProperties: false,
   properties: {
     data: address,
-    pagination: { type: 'object' },
+    pagination,
   },
   required: ['data'],
   type: 'object',
@@ -58,17 +121,70 @@ export const addressTransactions = {
     data: {
       items: {
         properties: {
-          id: { type: 'string' },
+          amount: {
+            properties: {
+              amount: { type: 'string' },
+              currency: { enum: collection.map((item) => item.currency), type: 'string' },
+            },
+            required: ['amount', 'currency'],
+            type: 'object',
+          },
+          created_at: {
+            pattern: composePattern('DATE_ISO'),
+            type: 'string',
+          },
+          description: { type: ['null', 'string'] },
+          from: {
+            properties: {
+              id: {
+                pattern: composePattern('UUID'),
+                type: 'string',
+              },
+              resource: { enum: ['user'], type: 'string' },
+            },
+            required: ['id', 'resource'],
+            type: 'object',
+          },
+          id: {
+            pattern: composePattern('UUID'),
+            type: 'string',
+          },
+          native_amount: {
+            properties: {
+              amount: { type: 'string' },
+              currency: { enum: collection.map((item) => item.currency), type: 'string' },
+            },
+            required: ['amount', 'currency'],
+            type: 'object',
+          },
           network: {
             properties: {
-              name: { enum: ['bitcoin', 'ethereum'], type: 'string' },
+              name: { enum: collection.map((item) => item.network), type: 'string' },
               status: { enum: ['off_blockchain'], type: 'string' },
             },
             required: ['name', 'status'],
             type: 'object',
           },
+          resource: { enum: ['transaction'], type: 'string' },
+          resource_path: {
+            pattern: composePattern(
+              {
+                account_uuid: 'UUID',
+                address_uuid: 'UUID',
+              },
+              config.PATH.ADDRESS_TRANSACTIONS,
+            ),
+            type: 'string',
+          },
           status: { enum: ['completed'], type: 'string' },
-          type: { enum: ['send'], type: 'string' },
+          type: {
+            enum: ['advanced_trade_fill', 'buy', 'request', 'transfer', 'send'],
+            type: 'string',
+          },
+          updated_at: {
+            pattern: composePattern('DATE_ISO'),
+            type: 'string',
+          },
         },
         required: ['id', 'network', 'status', 'type'],
         type: 'object',
@@ -76,8 +192,8 @@ export const addressTransactions = {
       minItems: 0,
       type: 'array',
     },
-    pagination: { type: 'object' },
+    pagination,
   },
-  required: ['data'],
+  required: ['pagination'],
   type: 'object',
 };
