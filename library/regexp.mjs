a22c03717b69fe343e8a102d9fc4d6b725ad9a4d
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-02-08 16:49:52 +0500

    feat(library/regexp.mjs): create library for regular expressions

---
+++ library/regexp.mjs
@@ -0,0 +1,34 @@
+/**!
+ * Regular expression related methods for common application use cases.
+ *
+ * @module library/regexp
+ */
+
+import { REGEXP } from '#lib/constants.mjs';
+import { stringifyPath } from '#lib/fetch.mjs';
+
+/**
+ * Compose JSON-Schema pattern from a regular expression by its name.
+ * @param {Object<string, string> | string} target Map-like object of template expressions to regular expression names or a single regular expression name .
+ * @param {string} [template] String as template literal.
+ * @returns {string} JSON-Schema pattern.
+ */
+export const composePattern = (target, template) => {
+  let pattern;
+
+  if (target instanceof Object) {
+    pattern =
+      '^' +
+      stringifyPath(
+        template,
+        Object.entries(target)
+          .map((entry) => ({
+            [entry[0]]: String(REGEXP[entry[1]]).replaceAll('\\-', '-').slice(2, -2),
+          }))
+          .reduce((accum, item) => ((accum = { ...accum, ...item }), accum), {}),
+      ) +
+      '$';
+  } else pattern = String(REGEXP[target]).replaceAll('\\-', '-').slice(1, -1);
+
+  return pattern;
+};
