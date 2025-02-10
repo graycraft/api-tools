46d8d5e66462bc251c73afeaf71b800ab190d30a
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-02-10 20:42:23 +0500

    feat(regexp.mjs): allow regexp pass to `composePattern`

--- library/regexp.mjs
+++ library/regexp.mjs
@@ -9,7 +9,8 @@ import { stringifyPath } from '#lib/fetch.mjs';
 
 /**
  * Compose JSON-Schema pattern from a regular expression by its name.
- * @param {Object<string, string> | string} target Map-like object of template expressions to regular expression names or a single regular expression name .
+ * @param {Object<string, string> | RegExp | string} target Map-like object of template expressions
+ *   to regular expression names, regular expression or a regular expression name from constants.
  * @param {string} [template] String as template literal.
  * @returns {string} JSON-Schema pattern.
  */
@@ -17,18 +18,24 @@ export const composePattern = (target, template) => {
   let pattern;
 
   if (target instanceof Object) {
-    pattern =
-      '^' +
-      stringifyPath(
-        template,
-        Object.entries(target)
-          .map((entry) => ({
-            [entry[0]]: String(REGEXP[entry[1]]).replaceAll('\\-', '-').slice(2, -2),
-          }))
-          .reduce((accum, item) => ((accum = { ...accum, ...item }), accum), {}),
-      ) +
-      '$';
-  } else pattern = String(REGEXP[target]).replaceAll('\\-', '-').slice(1, -1);
+    if (target instanceof RegExp) {
+      pattern = String(target).replaceAll('\\-', '-').slice(1, -1);
+    } else {
+      pattern =
+        '^' +
+        stringifyPath(
+          template,
+          Object.entries(target)
+            .map((entry) => ({
+              [entry[0]]: String(REGEXP[entry[1]]).replaceAll('\\-', '-').slice(2, -2),
+            }))
+            .reduce((accum, item) => ((accum = { ...accum, ...item }), accum), {}),
+        ) +
+        '$';
+    }
+  } else {
+    pattern = String(REGEXP[target]).replaceAll('\\-', '-').slice(1, -1);
+  }
 
   return pattern;
 };
