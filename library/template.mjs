99b8ac4f03d9da1101d2186fbdde0793ecb3d979
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-03-08 03:43:37 +0500

    fix(library/template.mjs): clarify text for `required` template

--- library/template.mjs
+++ library/template.mjs
@@ -7,4 +7,4 @@
 export const fallback = (param) => `Falling back to "${param}". `;
 export const invalid = (param) => `Parameter \`${param}\` is not valid. `;
 export const optional = (param) => `Optional parameter \`${param}\` is not valid. `;
-export const required = (param) => `Required parameter \`${param}\` is not valid. `;
+export const required = (param) => `Required parameter \`${param}\` is not valid or undefined. `;
