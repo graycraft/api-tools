07e49b1e74daac9d1d24b55380c3ccbc8aa7e986
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-03-09 19:21:41 +0500

    fix(coinbase/parse/map.mjs): check `json` is instance of `Object`

--- response/coinbase/parse/map.mjs
+++ response/coinbase/parse/map.mjs
@@ -11,45 +11,49 @@
  *   key: string | string[],
  *   list: string | string[],
  * }} options
- * @returns {Response}
+ * @returns {Response | null}
  */
 const map = (json, { key, list }) => {
   let data, items;
 
-  if (typeof key === 'string' && typeof list === 'string') {
-    items = json[list].map((item) => ({
-      [key]: item[key],
-    }));
-    data = {
-      ...json,
-      [list]: items,
-    };
-  } else if (key instanceof Array && typeof list === 'string') {
-    items = json[list].map((item) => ({
-      [key[0]]: item[key[0]],
-      [key[1]]: item[key[1]],
-      [key[2]]: item[key[2]],
-    }));
-    data = {
-      ...json,
-      [list]: items,
-    };
-  } else if (typeof key === 'object' && typeof list === 'object') {
-    items = json.result[list[0]].map((item1) => ({
-      [key[0]]: item1[key[0]],
-      [list[1]]: item1[list[1]].map((item2) => ({
-        [key[1]]: item2[key[1]],
-      })),
-    }));
-    data = {
-      ...json,
-      result: {
-        [list[0]]: items,
-      },
-    };
+  if (json instanceof Object) {
+    if (typeof key === 'string' && typeof list === 'string') {
+      items = json[list].map((item) => ({
+        [key]: item[key],
+      }));
+      data = {
+        ...json,
+        [list]: items,
+      };
+    } else if (key instanceof Array && typeof list === 'string') {
+      items = json[list].map((item) => ({
+        [key[0]]: item[key[0]],
+        [key[1]]: item[key[1]],
+        [key[2]]: item[key[2]],
+      }));
+      data = {
+        ...json,
+        [list]: items,
+      };
+    } else if (typeof key === 'object' && typeof list === 'object') {
+      items = json.result[list[0]].map((item1) => ({
+        [key[0]]: item1[key[0]],
+        [list[1]]: item1[list[1]].map((item2) => ({
+          [key[1]]: item2[key[1]],
+        })),
+      }));
+      data = {
+        ...json,
+        result: {
+          [list[0]]: items,
+        },
+      };
+    }
+
+    return data;
   }
 
-  return data;
+  return null;
 };
 
 export default map;
