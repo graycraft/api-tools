88c65162e418a79f634fcb82b236e58490632435
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-03-09 19:42:07 +0500

    feat(types/response/coinbase.js): detailing type definition

---
+++ types/response/coinbase.js
@@ -0,0 +1,64 @@
+/**
+ * @typedef {import("types/response/coinbase/address/all.d.js").default} AddressAll
+ * @typedef {import("types/response/coinbase/address/new.d.js").default} AddressNew
+ * @typedef {import("types/response/coinbase/address/one.d.js").default} AddressOne
+ * @typedef {import("types/response/coinbase/address/transactions.d.js").default} AddressTransactions
+ * @typedef {import("types/response/coinbase/currency/all.d.js").default} CurrencyAll
+ * @typedef {import("types/response/coinbase/currency/one.d.js").default} CurrencyOne
+ * @typedef {import("types/response/coinbase/deposit/crypto-all.d.js").default} DepositCryptoAll
+ * @typedef {import("types/response/coinbase/deposit/crypto-one.d.js").default} DepositCryptoOne
+ * @typedef {import("types/response/coinbase/deposit/fiat-all.d.js").default} DepositFiatAll
+ * @typedef {import("types/response/coinbase/market/all.d.js").default} MarketAll
+ * @typedef {import("types/response/coinbase/market/one.d.js").default} MarketOne
+ * @typedef {import("types/response/coinbase/market/tickers.d.js").default} MarketTickers
+ * @typedef {import("types/response/coinbase/order/all.d.js").default} OrderAll
+ * @typedef {import("types/response/coinbase/order/book.d.js").default} OrderBook
+ * @typedef {import("types/response/coinbase/order/cancel.d.js").default} OrderCancel
+ * @typedef {import("types/response/coinbase/order/limit-buy.d.js").default} OrderLimitBuy
+ * @typedef {import("types/response/coinbase/order/limit-sell.d.js").default} OrderLimitSell
+ * @typedef {import("types/response/coinbase/order/market-buy.d.js").default} OrderMarketBuy
+ * @typedef {import("types/response/coinbase/order/market-sell.d.js").default} OrderMarketSell
+ * @typedef {import("types/response/coinbase/order/one.d.js").default} OrderOne
+ * @typedef {import("types/response/coinbase/product/candles.d.js").default} ProductCandles
+ * @typedef {import("types/response/coinbase/transaction/all.d.js").default} TransactionAll
+ * @typedef {import("types/response/coinbase/transaction/one.d.js").default} TransactionOne
+ * @typedef {import("types/response/coinbase/user/account-all.d.js").default} UserAccountAll
+ * @typedef {import("types/response/coinbase/user/account-one.d.js").default} UserAccountOne
+ * @typedef {import("types/response/coinbase/user/portfolio-all.d.js").default} UserPortfolioAll
+ * @typedef {import("types/response/coinbase/user/portfolio-one.d.js").default} UserPortfolioOne
+ * @typedef {import("types/response/coinbase/withdraw/all.d.js").default} WithdrawAll
+ * @typedef {import("types/response/coinbase/withdraw/new.d.js").default} WithdrawNew
+ * @typedef {import("types/response/coinbase/withdraw/one.d.js").default} WithdrawOne
+ * @typedef {
+ *   | AddressAll
+ *   | AddressNew
+ *   | AddressOne
+ *   | AddressTransactions
+ *   | CurrencyAll
+ *   | CurrencyOne
+ *   | DepositCryptoAll
+ *   | DepositCryptoOne
+ *   | DepositFiatAll
+ *   | MarketAll
+ *   | MarketOne
+ *   | MarketTickers
+ *   | OrderAll
+ *   | OrderBook
+ *   | OrderCancel
+ *   | OrderLimitBuy
+ *   | OrderLimitSell
+ *   | OrderMarketBuy
+ *   | OrderMarketSell
+ *   | OrderOne
+ *   | ProductCandles
+ *   | TransactionAll
+ *   | TransactionOne
+ *   | UserAccountAll
+ *   | UserAccountOne
+ *   | UserPortfolioAll
+ *   | UserPortfolioOne
+ *   | WithdrawAll
+ *   | WithdrawNew
+ *   | WithdrawOne
+ * } default
+ */
