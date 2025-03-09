48e9e619842bb548566581ccc0ca1dd8d5daf52c
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-03-09 19:38:15 +0500

    feat(types/response/bybit.js): detailing type definition

---
+++ types/response/bybit.js
@@ -0,0 +1,78 @@
+/**
+ * @typedef {import("./bybit/account/information.d.js").default} AccountInformation
+ * @typedef {import("./bybit/account/wallets.d.js").default} AccountWallets
+ * @typedef {import("./bybit/balance/all.d.js").default} BalanceAll
+ * @typedef {import("./bybit/balance/information.d.js").default} BalanceInformation
+ * @typedef {import("./bybit/balance/one.d.js").default} BalanceOne
+ * @typedef {import("./bybit/currency/all.d.js").default} CurrencyAll
+ * @typedef {import("./bybit/currency/network-all.d.js").default} CurrencyNetworkAll
+ * @typedef {import("./bybit/currency/network-one.d.js").default} CurrencyNetworkOne
+ * @typedef {import("./bybit/currency/one.d.js").default} CurrencyOne
+ * @typedef {import("./bybit/deposit/all.d.js").default} DepositAll
+ * @typedef {import("./bybit/deposit/new-master.d.js").default} DepositNewMaster
+ * @typedef {import("./bybit/deposit/new-sub.d.js").default} DepositNewSub
+ * @typedef {import("./bybit/deposit/one.d.js").default} DepositOne
+ * @typedef {import("./bybit/key/information.d.js").default} KeyInformation
+ * @typedef {import("./bybit/market/history.d.js").default} MarketHistory
+ * @typedef {import("./bybit/market/information.d.js").default} MarketInformation
+ * @typedef {import("./bybit/market/tickers.d.js").default} MarketTickers
+ * @typedef {import("./bybit/order/all.d.js").default} OrderAll
+ * @typedef {import("./bybit/order/book.d.js").default} OrderBook
+ * @typedef {import("./bybit/order/cancel-all.d.js").default} OrderCancelAll
+ * @typedef {import("./bybit/order/cancel-one.d.js").default} OrderCancelOne
+ * @typedef {import("./bybit/order/history-all.d.js").default} OrderHistoryAll
+ * @typedef {import("./bybit/order/history-one.d.js").default} OrderHistoryOne
+ * @typedef {import("./bybit/order/limit-buy.d.js").default} OrderLimitBuy
+ * @typedef {import("./bybit/order/limit-sell.d.js").default} OrderLimitSell
+ * @typedef {import("./bybit/order/market-buy.d.js").default} OrderMarketBuy
+ * @typedef {import("./bybit/order/market-sell.d.js").default} OrderMarketSell
+ * @typedef {import("./bybit/order/one.d.js").default} OrderOne
+ * @typedef {import("./bybit/trade/history-all.d.js").default} TradeHistoryAll
+ * @typedef {import("./bybit/trade/history-one.d.js").default} TradeHistoryOne
+ * @typedef {import("./bybit/trade/rates.d.js").default} TradeRates
+ * @typedef {import("./bybit/transfer/all.d.js").default} TransferAll
+ * @typedef {import("./bybit/transfer/internal.d.js").default} TransferInternal
+ * @typedef {import("./bybit/transfer/one.d.js").default} TransferOne
+ * @typedef {import("./bybit/withdraw/all.d.js").default} WithdrawAll
+ * @typedef {import("./bybit/withdraw/new.d.js").default} WithdrawNew
+ * @typedef {import("./bybit/withdraw/one.d.js").default} WithdrawOne
+ * @typedef {
+ *   | AccountInformation
+ *   | AccountWallets
+ *   | BalanceAll
+ *   | BalanceInformation
+ *   | BalanceOne
+ *   | CurrencyAll
+ *   | CurrencyNetworkAll
+ *   | CurrencyNetworkOne
+ *   | CurrencyOne
+ *   | DepositAll
+ *   | DepositNewMaster
+ *   | DepositNewSub
+ *   | DepositOne
+ *   | KeyInformation
+ *   | MarketHistory
+ *   | MarketInformation
+ *   | MarketTickers
+ *   | OrderAll
+ *   | OrderBook
+ *   | OrderCancelAll
+ *   | OrderCancelOne
+ *   | OrderHistoryAll
+ *   | OrderHistoryOne
+ *   | OrderLimitBuy
+ *   | OrderLimitSell
+ *   | OrderMarketBuy
+ *   | OrderMarketSell
+ *   | OrderOne
+ *   | TradeHistoryAll
+ *   | TradeHistoryOne
+ *   | TradeRates
+ *   | TransferAll
+ *   | TransferInternal
+ *   | TransferOne
+ *   | WithdrawAll
+ *   | WithdrawNew
+ *   | WithdrawOne
+ * } default
+ */
