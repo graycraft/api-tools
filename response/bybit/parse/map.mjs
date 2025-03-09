f6c649ef11d8acd666bd3850a10d0f4865c1ac33
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-03-09 19:06:41 +0500

    fix(bybit/parse/map.mjs): check `json` is instance of `Object`

--- response/bybit/parse/map.mjs
+++ response/bybit/parse/map.mjs
@@ -11,37 +11,41 @@
  *   key: string | string[],
  *   list: string | string[],
  * }} options
- * @returns {Response}
+ * @returns {Response | null}
  */
 const map = (json, { key, list }) => {
   let data, items;
 
-  if (typeof key === 'string' && typeof list === 'string') {
-    items = json.result[list]?.map((item) => ({
-      [key]: item[key],
-    }));
-    data = {
-      ...json,
-      result: {
-        [list]: items,
-      },
-    };
-  } else if (key instanceof Array && list instanceof Array) {
-    items = json.result[list[0]].map((item1) => ({
-      [key[0]]: item1[key[0]],
-      [list[1]]: item1[list[1]].map((item2) => ({
-        [key[1]]: item2[key[1]],
-      })),
-    }));
-    data = {
-      ...json,
-      result: {
-        [list[0]]: items,
-      },
-    };
+  if (json instanceof Object) {
+    if (typeof key === 'string' && typeof list === 'string') {
+      items = json.result[list]?.map((item) => ({
+        [key]: item[key],
+      }));
+      data = {
+        ...json,
+        result: {
+          [list]: items,
+        },
+      };
+    } else if (key instanceof Array && list instanceof Array) {
+      items = json.result[list[0]].map((item1) => ({
+        [key[0]]: item1[key[0]],
+        [list[1]]: item1[list[1]].map((item2) => ({
+          [key[1]]: item2[key[1]],
+        })),
+      }));
+      data = {
+        ...json,
+        result: {
+          [list[0]]: items,
+        },
+      };
+    }
+
+    return data;
   }
 
-  return data;
+  return null;
 };
 
 export default map;
